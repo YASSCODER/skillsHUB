1d767fbb8dbd597a2e8ea2d912eb6839a83c90a5
chaymaSm <samaalichayma99@gmail.com>
2025-03-18 00:33:29 +0100

    change++

--- src/modules/challenge/api/challenge.routes.ts
+++ src/modules/challenge/api/challenge.routes.ts
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import { ChallengeController } from "./challenge.controller";
+import catchAsync from "../../../common/utils/catch-async.utils";
 
 const router = Router();
 const challengeController = new ChallengeController();
 
-/*router.post("/", (req, res, next) => challengeController.createChallenge(req, res).catch(next));
-router.get("/", (req, res, next) => challengeController.getAllChallenges(req, res).catch(next));
-router.get("/:id", (req, res, next) => challengeController.getChallengeById(req, res).catch(next));
-router.put("/:id", (req, res, next) => challengeController.updateChallenge(req, res).catch(next));
-router.delete("/:id", (req, res, next) => challengeController.deleteChallenge(req, res).catch(next));
+router.post("/",  catchAsync(challengeController.createChallenge));
+router.get("/", catchAsync(challengeController.getAllChallenges));
+router.get("/:id",catchAsync(challengeController.getChallengeById));
+router.put("/:id", catchAsync(challengeController.updateChallenge));
+router.delete("/:id", catchAsync(challengeController.deleteChallenge));
 
-export default router;*/
+export default router;
