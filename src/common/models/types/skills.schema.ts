e2360680a8bb7fa1c179f76b35754813e67bc40d
Manel1804 <168225965+Manel1804@users.noreply.github.com>
2025-05-26 20:25:31 +0100

    résolution des conflits après stash

--- src/common/models/types/skills.schema.ts
+++ src/common/models/types/skills.schema.ts
@@ -2,11 +2,13 @@ import mongoose, { Schema } from "mongoose";
 import { BaseSchema } from "../base-model.schema";
 import { ISkill } from "../interface/skills.interface";
 import { userInfo } from "os";
+import { FLOAT } from "sequelize";
 export const SkillsSchema: Schema = new Schema<ISkill>({
   name: { type: String, required: true },
   description: { type: String, required: true },
   category: {type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true},
   userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  valeur:{type:Float64Array, default: 0},
   users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
   createdAt: { type: Date, default: Date.now }
 });
